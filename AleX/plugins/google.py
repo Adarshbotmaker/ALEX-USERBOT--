58f3c68a619505ca92c4dbbca5e723fb1350b96a
Adarshbotmaker <89734618+Adarshbotmaker@users.noreply.github.com>
2021-10-30 16:13:27 +0530

    Create google.py

---
+++ AleX/plugins/google.py
@@ -0,0 +1,14 @@
+from userbot.utils import admin_cmd 
+ 
+ 
+ 
+ 
+@borg.on(admin_cmd(pattern=".alive")) 
+ 
+ 
+ 
+async def gn(event):
+ 
+ 
+ 
+    await event.edit("Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware...

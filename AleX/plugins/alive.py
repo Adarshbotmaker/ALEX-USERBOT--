ded39480159e017853c9369cef731af3984b74ea
Adarshbotmaker <89734618+Adarshbotmaker@users.noreply.github.com>
2021-10-30 16:09:35 +0530

    Create alive.py

---
+++ AleX/plugins/alive.py
@@ -0,0 +1,26 @@
+from userbot.utils import admin_cmd 
+ 
+ 
+ 
+ 
+@borg.on(admin_cmd(pattern=".alive")) 
+ 
+ 
+ 
+async def gn(event):
+ 
+ 
+ 
+    await event.edit("ALEXBOT IS ALIVE🔥🔥
+
+━━━━━━━━━━━━━━━━━━━━
+
+                 👑𝐌𝐀𝐒𝐓𝐄𝐑👑
+  『😈ALEX BOY😈』
+
+┏━━━━━━━━━━━━━━━━━━━
+┣•➳➠ Telethon: 1.0.0 
+┣•➳➠ Version: 1.0.0
+┣•➳➠ Sudo: True
+┣•➳➠ Channel: [HERE](t.me/alex_userbot)
+┣•➳➠ Creator: 🔥🔥[ADARSH](t.me/adarshop_xd")

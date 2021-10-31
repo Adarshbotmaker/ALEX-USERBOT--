c7d23cefc700e7716f6bdf610776d59610649e01
Adarshbotmaker <89734618+Adarshbotmaker@users.noreply.github.com>
2021-10-31 11:04:31 +0530

    Update angry.py

--- AleX/plugins/angry.py
+++ AleX/plugins/angry.py
@@ -8,12 +8,12 @@ async def _(event):
     if event.fwd_from:
         return
     event = await edit_or_reply(event, "𝙸 𝚊𝚖 𝚊𝚗𝚐𝚛𝚢 ")
-    deq = deque(list("😡🔥😡🔥🤬🔥🤬🔥🤬𝚅2.𝙾🔥😡🔥🤬😡🔥🤬🤬🔥😡"))
+    deq = deque(list("😁🔥👿🇮🇳🤬😈🤬🔥👍𝚅1.𝙾🔥😡🤔🤬😡🔥😤🙄😒🤬🔥😡"))
     for _ in range(100):
         await asyncio.sleep(0.3)
         await event.edit("".join(deq))
         deq.rotate(1)
         
 CmdHelp("angry").add_command(
-  "🤬", None, "υѕє αи∂ ѕєє"
+  "🤬", None, "USe ANd SEe"
 ).add()
